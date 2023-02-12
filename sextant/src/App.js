6a2848541a0521a6c4712a75301908b2fe2050db
César Liendo <cesargliendo@gmail.com>
2023-02-11 23:20:28 -0500

    minor changes

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import Banner from './components/Banner';
-import Card from './components/Card';
-import Exhibit from './components/Exhibit';
-import IPdisplay from './components/IPdisplay';
-import Latency from './components/Latency';
-import ProgressBar from './components/ProgressBar';
-import StatisticaPro from './components/StatisticaPro';
-import Weather from './components/Weather';
-import Clock from './components/Clock';
-import { useEffect, useRef } from 'react';
+import "./App.css";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Exhibit from "./components/Exhibit";
+import IPdisplay from "./components/IPdisplay";
+import Latency from "./components/Latency";
+import StatisticaPro from "./components/StatisticaPro";
+import Weather from "./components/Weather";
+import Clock from "./components/Clock";
+import { useEffect, useRef } from "react";
 
 var client;
 var W3CWebSocket;
@@ -16,95 +15,93 @@ var init = false;
 var connected = false;
 var data;
 
+const connect = (callback) => {
+	if (!init) {
+		init = true;
+		W3CWebSocket = require("websocket").w3cwebsocket;
+		// client = new W3CWebSocket('ws://localhost:55460/', null);
+		client = new W3CWebSocket("ws://localhost:55460/", "echo-protocol");
+	}
+	client.onclose = function () {
+		console.log("Connection Closed");
+	};
+	client.onerror = function () {
+		console.log("Connection Error");
+	};
+	client.onopen = function () {
+		console.log("WebSocket Client Connected");
+		connected = true;
+	};
+	client.onmessage = function (e) {
+		// console.log('message:', JSON.parse(e.data));
+		data = JSON.parse(e.data);
+	};
+};
 
-const connect = (callback)=>{
-  if(!init){
-      init=true;
-      W3CWebSocket = require('websocket').w3cwebsocket;
-      // client = new W3CWebSocket('ws://localhost:55460/', null);
-      client = new W3CWebSocket('ws://localhost:55460/', 'echo-protocol');
-  }
-  client.onclose = function() {
-      console.log('Connection Closed');
-  }
-  client.onerror = function() {
-      console.log('Connection Error');
-  }; 
-  client.onopen = function() {
-      console.log('WebSocket Client Connected');
-      connected = true;
-  };
-  client.onmessage = function(e) {
-      // console.log('message:', JSON.parse(e.data));
-      data = JSON.parse(e.data);
-  };
-}
-
-const close = ()=>{
-  if(connected===true){
-    client.close();
-    connected=init=false;
-  }
-}
-
+const close = () => {
+	if (connected === true) {
+		client.close();
+		connected = init = false;
+	}
+};
 
 function App() {
-  const statisticaData = useRef({
-        hostName: "Connecting...",
-        cpu: 0,
-        gpu: 0,
-        memory: 0,
-        temp_cpu: 0
-  });
+	const statisticaData = useRef({
+		hostName: "Connecting...",
+		cpu: 0,
+		gpu: 0,
+		memory: 0,
+		temp_cpu: 0,
+	});
 
-  let interval;
-  useEffect(()=>{
-    //-------------------------------
-    // Setup
-    //-------------------------------
-    console.log("\n\nApp started.");
-    if(!init) connect();
-    interval = setInterval(()=>{
-      console.log("APP:Timeout:",statisticaData.current);
-      statisticaData.current = {...data};
-    },5000);
+	let interval;
+	useEffect(() => {
+		//-------------------------------
+		// Setup
+		//-------------------------------
+		console.log("\n\nApp started.");
+		if (!init) connect();
+		interval = setInterval(() => {
+			console.log("APP:Timeout:", statisticaData.current);
+			statisticaData.current = { ...data };
+		}, 5000);
 
-    //-------------------------------
-    //Cleanup
-    //-------------------------------
-    return ()=> {
-      console.log("App removed");
-      clearInterval(interval);
-      close();
-  }
-  });
+		//-------------------------------
+		//Cleanup
+		//-------------------------------
+		return () => {
+			console.log("App removed");
+			clearInterval(interval);
+			close();
+		};
+	});
 
-  //-------------------------------
-  //Render
-  //-------------------------------
-  return (
-    <div className="App">
-      <Banner title={document.title}/>
-      {/* {pageButtons} */}
-      <Exhibit className='exhibit' /*title={pageTitle}*/>
-        <Card>
-            <h2>Network</h2>
-            <IPdisplay/>
-            <IPdisplay ipv6={true}/>
-            <Latency/>
-        </Card>
-        <Card>
-          <StatisticaPro data={statisticaData}/>
-        </Card>
-        <Card>
-          <Weather/>
-        </Card>
-        <Card>
-          <Clock/>
-        </Card>
-      </Exhibit>
-    </div>
-  );
+	//-------------------------------
+	//Render
+	//-------------------------------
+	return (
+		<div className="App">
+			<Banner title={document.title} />
+			{/* {pageButtons} */}
+			<Exhibit className="exhibit" /*title={pageTitle}*/>
+				<Card>
+					<h2>Network</h2>
+					<IPdisplay />
+					<IPdisplay ipv6={true} />
+					<Latency />
+				</Card>
+				<Card>
+					<StatisticaPro data={statisticaData} />
+				</Card>
+				<Card>
+					<Weather />
+				</Card>
+				<Card>
+					<Clock />
+				</Card>
+			</Exhibit>
+		</div>
+	);
 }
 
 export default App;
--- sextant/src/components/StatisticaPro.js
+++ sextant/src/components/StatisticaPro.js
@@ -2,52 +2,37 @@ import { memo, useEffect, useState, useRef } from "react";
 import ProgressBar from "./ProgressBar";
 import StatItem from "./StatItem";
 
-
-
-
-
 const StatisticaPro = (props) => {
-    const [renderData,setRenderData] = useState(props.data.current);
-    
-    
-    let timeout;
-    useEffect(() => {
-        // console.log('in useEffect');
-      timeout = setInterval(() => {
-        console.log('COMPONENT:',props.data.current);
-        setRenderData(props.data.current);
-      }, 5200);
+	const [renderData, setRenderData] = useState(props.data.current);
 
-        return ()=> clearInterval(timeout);
-    });
-    
-    let temp_color = renderData.temp_cpu >= 85 ? 'warning': undefined;
-    temp_color = renderData.temp_cpu >= 95 ? 'danger': temp_color;
+	let timeout;
+	useEffect(() => {
+		// console.log('in useEffect');
+		timeout = setInterval(() => {
+			console.log("COMPONENT:", props.data.current);
+			setRenderData(props.data.current);
+		}, 5200);
 
+		return () => clearInterval(timeout);
+	});
 
-    return (
-        <div>
-            <h2>{renderData.hostName}</h2>
-            <ProgressBar percent={ renderData.cpu }
-                         label='CPU Load'
-                         unit='%'
-                         />
-            <ProgressBar percent={ renderData.gpu }
-                         label='GPU Load'
-                         unit='%'
-                         />
-            <ProgressBar percent={ renderData.memory }
-                         label='Memory'
-                         unit='%'
-                         />
-            <ProgressBar percent={ renderData.temp_cpu * 0.8 }
-                         value={ renderData.temp_cpu }
-                        //  type={temp_color}
-                         label='CPU Temp'
-                         unit='°C'
-                         />
-        </div>
-    );
-    }
-    export default StatisticaPro;
+	let temp_color = renderData.temp_cpu >= 85 ? "warning" : undefined;
+	temp_color = renderData.temp_cpu >= 95 ? "danger" : temp_color;
 
+	return (
+		<div>
+			<h2>{renderData.hostName}</h2>
+			<ProgressBar percent={renderData.cpu} label="CPU Load" unit="%" />
+			<ProgressBar percent={renderData.gpu} label="GPU Load" unit="%" />
+			<ProgressBar percent={renderData.memory} label="Memory" unit="%" />
+			<ProgressBar
+				percent={renderData.temp_cpu * 0.8}
+				value={renderData.temp_cpu}
+				type={temp_color}
+				label="CPU Temp"
+				unit="°C"
+			/>
+		</div>
+	);
+};
+export default StatisticaPro;
